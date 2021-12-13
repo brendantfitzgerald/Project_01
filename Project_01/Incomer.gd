3ab21e3a06b72db03f36f964fe068231d0abcaff
FitzⓋ <17690974+brendantfitzgerald@users.noreply.github.com>
2021-12-13 17:56:41 -0600

    Movement, autofire, and controls updated.
    Added keyboard controls
    Changed boundaries for Player to be based on half-width of Sprite
    Added wraparound movement for Player
    Added Autofire Timer Node
    Added Autofire to Player Script

--- Project_01/Incomer.gd
+++ Project_01/Incomer.gd
@@ -3,16 +3,21 @@ extends Node2D
 
 # Declare member variables here. Examples:
 var health = 3
-
+var EnemyNumber = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_to_group("Enemies")
+	EnemyNumber = get_tree().get_nodes_in_group("Enemies").size()
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.position.y += 3
+	if EnemyNumber > 3:
+		self.position.x += rand_range(-3,3)
 	pass
 
 func area_entered(otherArea):
--- Project_01/Player.gd
+++ Project_01/Player.gd
@@ -4,18 +4,32 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var width = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	width = ($Sprite.texture.get_width() * $Sprite.scale.x * scale.x) / 2
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position.x = get_global_mouse_position().x
-	
+	# Movement
+	if Input.is_action_pressed("ui_right"):
+		position.x += 5
+	if Input.is_action_pressed("ui_left"): 
+		position.x -= 5
+	# Boundaries
+	if position.x <= width:
+		position.x = 479 - width
+	if position.x >= 480 - width:
+		position.x = width
+	# Weapon
 	if Input.is_action_just_pressed("fire_projectile"):
-		go.spawn_instance("Projectile", self.position.x, self.position.y)
-	
+		go.spawn_instance("Projectile", position.x, position.y)
 	pass
+
+
+func _on_AutoShotTimer_timeout():
+	go.spawn_instance("Projectile", position.x, position.y)
+	pass # Replace with function body.
